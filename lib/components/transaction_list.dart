981a930fccb3a2bd1ab4f94c4a19cd94f61c3570
munizdiogo <diogomuniz.web@gmail.com>
2020-05-21 20:43:18 -0300

    Aula101-Fontes Personalizadas e Theme.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -36,8 +36,7 @@ class TransactionList extends StatelessWidget {
                   children: [
                     Text(
                       tr.title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      style: Theme.of(context).textTheme.headline6
                     ),
                     Text(
                       DateFormat('d MMM y').format(tr.data),
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,25 @@ class ExpensesApp extends StatelessWidget {
       home: MyHomePage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
-        accentColor: Colors.amber
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          )
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+
+            ),
+          )
+        ),
       ),
     );
   }
@@ -76,7 +94,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas Pessoais'),
+        title: Text('Despesas Pessoais',
+        style: TextStyle(
+          fontFamily: 'OpenSans'
+          ),
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
