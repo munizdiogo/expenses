556febcb7b6f30ad86f248891c99250fd69f098b
munizdiogo <diogomuniz.web@gmail.com>
2020-05-21 16:59:50 -0300

    Aula98 - Delete transaction_user, Criação FormModal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:expenses/components/transaction_user.dart';
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:expenses/models/transaction.dart';
+import 'components/transaction_list.dart';
 
 main() => runApp(
   ExpensesApp(),
@@ -16,13 +19,65 @@ class ExpensesApp extends StatelessWidget {
 }
 
 
-class MyHomePage extends StatelessWidget {  
+class MyHomePage extends StatefulWidget {  
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      data: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      data: DateTime.now()
+    ),
+  ];
+
+  _openTransactionFormModal(BuildContext context){
+    showModalBottomSheet(
+      context: context, 
+      builder: (_){
+        return TransactionForm(null);
+      }
+    );
+  }
+
+  _addTransaction(String title, double value){
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      data: DateTime.now()
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context),
+          )
+          
+        ],
       ),
       body: SingleChildScrollView(
               child: Column(
@@ -36,10 +91,15 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser()
+            TransactionList(_transactions),
           ],
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
     );
   }
 }
