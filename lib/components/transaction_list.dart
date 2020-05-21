7550df44e2aff205959741938ebca4e7f5639a9c
munizdiogo <diogomuniz.web@gmail.com>
2020-05-21 16:22:10 -0300

    Aula 95 - Usando ListView.builder

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -13,57 +13,57 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-            child: Column(
-                children: transactions.map((tr) {
-                  return Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 10
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2
-                            )
-                          ),
-                          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                          child: Text(
-                            "R\$ ${tr.value.toStringAsFixed(2)}",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple
-                            ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index){
+          final tr = transactions[index];
+          return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2
+                      )
+                    ),
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Text(
+                      "R\$ ${tr.value.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16
                           ),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tr.title,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16
-                                ),
-                              ),
-                            Text(
-                              DateFormat('d MMM y').format(tr.data),
-                              style: TextStyle(
-                                color: Colors.grey
-                              ),
-                              )
-                          ],
+                      Text(
+                        DateFormat('d MMM y').format(tr.data),
+                        style: TextStyle(
+                          color: Colors.grey
+                        ),
                         )
-                      ],
-                    ),
-                  );
-                }).toList(),
+                    ],
+                  )
+                ],
               ),
-      ),
+            );
+          },
+        ),
     );
   }
 }
